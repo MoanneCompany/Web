8f26df3ea9dc3296eb531185dbf091ef8c5410c3
Moanne <122030777+MoanneCompany@users.noreply.github.com>
2023-11-05 16:47:34 -0300

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    if (localStorage.getItem('logged') === null) {
-        location.assign('MoanneCompany/web/logon/index.html');
+    if (localStorage.getItem('logged') == null) {
+        location.assign('https://moannecompany.github.io/web/logon');
     }
 
     
