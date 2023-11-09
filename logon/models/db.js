04157bf1311c825bfbb61a37f18566dd8f6908a4
Moanne <122030777+MoanneCompany@users.noreply.github.com>
2023-11-09 12:54:59 +0000

    a

--- logon/models/db.js
+++ logon/models/db.js
@@ -8,8 +8,8 @@ const sequelize = new Sequelize(moanne_users, 'root', 'Moanne1315114145031513161
 sequelize.authenticate()
 .then(() => {
     console.log('sequile = SUCCESS');
-}).catch(() => {
-    console.log('sequelize = ERROR');
+}).catch((error) => {
+    console.log('sequelize = ERROR: ' + error);
 });
 
 module.exports = sequelize;
