8a47705c4a2f6ede9e5b665522de6e51ecc4b90e
Moanne <122030777+MoanneCompany@users.noreply.github.com>
2023-11-09 20:07:06 +0000

    a

--- logon/index.js
+++ logon/index.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
 		start.style.fontSize = '12px';
 		text.style.fontSize = '9px';
 		h.style.marginTop = '0';
+		h.style.margin = '0';
 		btn.style.width = '150px';
 		btn.style.height = '27px';
 		btn.style.fontSize = '11px';
