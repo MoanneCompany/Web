9fec8d368c4a2357b47b3924d9a1c57056855e09
Moanne <122030777+MoanneCompany@users.noreply.github.com>
2023-11-09 13:18:19 +0000

    a

--- logon/models/db.js
+++ logon/models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(moanne_users, 'root', 'Moanne1315114145031513161141425!!', {
+const sequelize = new Sequelize('moanne_users', 'root', 'Moanne1315114145031513161141425!!', {
 	host: 'localhost',
 	dialect: 'mysql'
 });
