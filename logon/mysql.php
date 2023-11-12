b811e67754a8264dd45383027a706cdc7ca33ce4
Moanne <122030777+MoanneCompany@users.noreply.github.com>
2023-11-12 20:22:54 +0000

    a

---
+++ logon/mysql.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Moanne1315114145031513161141425!!";
+$dbname = "moanne_users";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+$user = ''
+
+?>
