959beacc9ba41bad0172170206c837c0638d627e
Moanne <122030777+MoanneCompany@users.noreply.github.com>
2023-11-12 11:46:32 +0000

    a

--- logon/index.js
+++ logon/index.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	var lang;
 
 	if (!mobile_check()) {
-		console.log("pc");
+		alert("pc");
 		body.style.alignItems = 'top';
 		body.style.justifyContent = 'top';
 		h.style.marginLeft = '50px';
